2834f42cb81fcfaeafa769f9227a7212d428a911
Pybfinger <pbrisson@codable.com>
2021-05-21 12:28:28 -0400

    Added name and ID parameters to addNetwork.

--- src/metamask/addNetwork.ts
+++ src/metamask/addNetwork.ts
@@ -1,9 +1,9 @@
 import { Page } from 'puppeteer';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const addNetwork = (page: Page, version?: string) => async (url: string): Promise<void> => {
+export const addNetwork = (page: Page, version?: string) => async (name: string, url: string, id: number): Promise<void> => {
   await page.bringToFront();
-  const networkSwitcher = await page.waitForSelector('.network-indicator');
+  const networkSwitcher = await page.waitForSelector('.network-display');
   await networkSwitcher.click();
   await page.waitForSelector('li.dropdown-menu-item');
   const networkIndex = await page.evaluate((network) => {
@@ -18,9 +18,13 @@ export const addNetwork = (page: Page, version?: string) => async (url: string):
   }, 'Custom RPC');
   const networkButton = (await page.$$('li.dropdown-menu-item'))[networkIndex];
   await networkButton.click();
-  const newRPCInput = await page.waitForSelector('input#new-rpc');
+  const nameInput = await page.waitForSelector('input#network-name');
+  await nameInput.type(url);
+  const newRPCInput = await page.waitForSelector('input#rpc-url');
   await newRPCInput.type(url);
-  const saveButton = await page.waitForSelector('button.settings-tab__rpc-save-button');
+  const chainIDInput = await page.waitForSelector('input#chainId');
+  await chainIDInput.type(String(id));
+  const saveButton = await page.waitForSelector('button.btn-secondary');
   await saveButton.click();
   const prevButton = await page.waitForSelector('img.app-header__metafox-logo');
   await prevButton.click();
